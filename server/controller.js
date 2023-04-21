0d9cd9cb986ab56ca6a78963e5d0435339c726c2
Derek Smith <dereksmith6@me.com>
2023-04-20 18:56:01 -0600

    updated assessment

--- server/controller.js
+++ server/controller.js
@@ -3,7 +3,7 @@ let database = []
 
 module.exports = {
     getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+        const compliments = ["Gee, you're a smart cookie!", "Cool Shirt!", "Your Javascript skills are stellar."];
       
         // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
